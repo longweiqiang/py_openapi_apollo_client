8d3f2bea29eee5ae84ae52fc028c2c8671c5928c
longweiqiang_weimob <weiqiang.long@weimob.com>
2020-12-03 15:55:18 +0800

    update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name = "py_openapi_apollo_client",
-    version = "1.0.0",
+    version = "1.0.1",
     author = "Weiqiang.long",
     description = "python-apollo客户端封装",
     long_description = long_description,
